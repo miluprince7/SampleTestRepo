0b3b8483d4484052ebebd8414377568b909544c4
HP <HP@PAVILION-JP>
2025-03-30 12:23:32 +0530

    Student program

---
+++ Sample1MavenProject/src/main/java/samplepack/Student.java
@@ -0,0 +1,11 @@
+package samplepack;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Milu");
+		System.out.println("College: Rajagiri");
+
+	}
+
+}

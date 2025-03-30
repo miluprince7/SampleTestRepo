8c843cbb370cfec21d357dd69d8f13bdbf9aef3c
HP <HP@PAVILION-JP>
2025-03-30 12:27:41 +0530

    Student detail modified

--- Sample1MavenProject/src/main/java/samplepack/Student.java
+++ Sample1MavenProject/src/main/java/samplepack/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		System.out.println("Name: Milu");
 		System.out.println("College: Rajagiri");
+		System.out.println("Branch: Computer Science");
 
 	}
 
